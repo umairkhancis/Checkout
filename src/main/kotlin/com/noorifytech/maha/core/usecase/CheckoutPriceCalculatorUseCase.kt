50c0dac6ee11dcde37c249de32bbbb4caeb485c3
Umair Khan <umairkhan.cis@gmail.com>
2020-05-30 16:26:24 +0400

    Implement CheckoutPriceCalculatorUseCase.

--- src/main/kotlin/com/noorifytech/maha/core/usecase/CheckoutPriceCalculatorUseCase.kt
+++ src/main/kotlin/com/noorifytech/maha/core/usecase/CheckoutPriceCalculatorUseCase.kt
@@ -1,8 +1,47 @@
 package com.noorifytech.maha.core.usecase
 
-class CheckoutPriceCalculatorUseCase(private val cart: List<String>) {
+import com.noorifytech.maha.core.domain.Product
+import com.noorifytech.maha.data.repository.ProductRepository
+import com.noorifytech.maha.data.repository.impl.ProductRepositoryImpl
+
+class CheckoutPriceCalculatorUseCase(
+        private val cart: List<String>,
+        private val productRepository: ProductRepository = ProductRepositoryImpl()
+) {
 
     fun calculate(): Float {
-        return 360f
+        var totalPrice = 0f
+        val products = productRepository.getProducts(cart)
+
+        val itemCountMap = itemCountMap(cart)
+        val productDetailsMap = productDetailsMap(products)
+
+        for (itemId in itemCountMap.keys) {
+            val product = productDetailsMap[itemId]
+            val price = product?.unitPrice?.times(itemCountMap[itemId]?.toFloat() ?: 0f) ?: 0f
+            totalPrice += price
+        }
+
+        return totalPrice
+    }
+
+    private fun itemCountMap(itemIds: List<String>): HashMap<String, Int> {
+        val map = mutableMapOf<String, Int>()
+
+        for (itemId in itemIds) {
+            map[itemId] = map[itemId]?.plus(1) ?: 1
+        }
+
+        return map as HashMap<String, Int>
+    }
+
+    private fun productDetailsMap(products: List<Product>): HashMap<String, Product> {
+        val map = mutableMapOf<String, Product>()
+
+        for (product in products) {
+            map[product.id] = product
+        }
+
+        return map as HashMap<String, Product>
     }
 }
--- src/test/kotlin/com/noorifytech/maha/core/usecase/CheckoutPriceCalculatorUseCaseTest.kt
+++ src/test/kotlin/com/noorifytech/maha/core/usecase/CheckoutPriceCalculatorUseCaseTest.kt
@@ -1,19 +1,56 @@
 package com.noorifytech.maha.core.usecase
 
+import com.noorifytech.maha.core.domain.Product
+import com.noorifytech.maha.data.repository.ProductRepository
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
 
 class CheckoutPriceCalculatorUseCaseTest {
 
+    @Mock
+    private lateinit var productRepository: ProductRepository
+
+    @BeforeEach
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+    }
+
     @Test
-    fun calculate() {
+    fun calculate_whenOneItemEach() {
         // Arrange
-        val cart = listOf("001", "002", "001", "004", "003")
+        val cart = listOf("001", "002", "003", "004")
+        Mockito.`when`(productRepository.getProducts(cart)).thenReturn(getProducts())
 
         // Act
-        val price = CheckoutPriceCalculatorUseCase(cart).calculate()
+        val price = CheckoutPriceCalculatorUseCase(cart, productRepository).calculate()
 
         // Assert
-        Assertions.assertThat(price).isEqualTo(360f)
+        Assertions.assertThat(price).isEqualTo(260f)
+    }
+
+    @Test
+    fun calculate_whenDiscountAppliesForOneItemOnly() {
+        // Arrange
+        val cart = listOf("001", "002", "002", "004", "003")
+        Mockito.`when`(productRepository.getProducts(cart)).thenReturn(getProducts())
+
+        // Act
+        val price = CheckoutPriceCalculatorUseCase(cart, productRepository).calculate()
+
+        // Assert
+        Assertions.assertThat(price).isEqualTo(340f)
+    }
+
+    private fun getProducts(): List<Product> {
+        return listOf(
+                Product("001", "Rolex", 100f),
+                Product("002", "Michael Kors", 80f),
+                Product("003", "Swatch", 50f),
+                Product("004", "Casio", 30f)
+        )
     }
 }
