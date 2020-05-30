fd0fb46abc878dc447da47ca7c7620d5ff63eaf6
Umair Khan <umairkhan.cis@gmail.com>
2020-05-31 01:34:08 +0400

    Add more tests for CheckoutPriceCalculatorUseCase.

--- src/test/kotlin/com/noorifytech/maha/core/usecase/CheckoutPriceCalculatorUseCaseTest.kt
+++ src/test/kotlin/com/noorifytech/maha/core/usecase/CheckoutPriceCalculatorUseCaseTest.kt
@@ -1,99 +1,115 @@
 package com.noorifytech.maha.core.usecase
 
 import com.noorifytech.maha.core.domain.Product
+import com.noorifytech.maha.core.usecase.impl.CheckoutPriceCalculatorUseCaseImpl
 import com.noorifytech.maha.data.repository.ProductRepository
+import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
+import java.math.BigDecimal
 
 class CheckoutPriceCalculatorUseCaseTest {
 
     @Mock
     private lateinit var productRepository: ProductRepository
+    private lateinit var checkoutPriceCalculatorUseCase: CheckoutPriceCalculatorUseCase
 
     @BeforeEach
     fun setup() {
         MockitoAnnotations.initMocks(this)
+        checkoutPriceCalculatorUseCase = CheckoutPriceCalculatorUseCaseImpl(productRepository)
     }
 
     @Test
-    fun calculate_whenOnlyOneItem() {
+    fun calculate_whenOnlyOneItem(): Unit = runBlocking {
         // Arrange
         val cart = listOf("001")
-        Mockito.`when`(productRepository.getProducts(cart)).thenReturn(listOf(Product("001", "Rolex", 100f)))
+        Mockito.`when`(productRepository.getProducts(cart))
+                .thenReturn(listOf(Product("001", "Rolex", BigDecimal(100))))
 
         // Act
-        val price = CheckoutPriceCalculatorUseCase(cart, productRepository).calculate()
+        val price = checkoutPriceCalculatorUseCase.calculate(cart)
 
         // Assert
-        Assertions.assertThat(price).isEqualTo(100f)
+        Assertions.assertThat(price).isEqualTo(BigDecimal(100))
+
+        Unit
     }
 
     @Test
-    fun calculate_whenDiscountedItemIsLessThanDiscountedQuantity() {
+    fun calculate_whenDiscountedItemIsLessThanDiscountedQuantity(): Unit = runBlocking {
         // Arrange
         val cart = listOf("001", "002")
         Mockito.`when`(productRepository.getProducts(cart))
                 .thenReturn(listOf(
-                        Product("001", "Rolex", 100f),
-                        Product("002", "Michael Kors", 80f, discountPrice = 120f, discountQty = 2)
-                ))
+                        Product("001", "Rolex", BigDecimal(100)),
+                        Product("002", "Michael Kors", BigDecimal(80), discountPrice = BigDecimal(120), discountQty = 2))
+                )
 
         // Act
-        val price = CheckoutPriceCalculatorUseCase(cart, productRepository).calculate()
+        val price = checkoutPriceCalculatorUseCase.calculate(cart)
 
         // Assert
-        Assertions.assertThat(price).isEqualTo(180f)
+        Assertions.assertThat(price).isEqualTo(BigDecimal(180))
+
+        Unit
     }
 
     @Test
-    fun calculate_whenOneItemEach() {
+    fun calculate_whenOneItemEach(): Unit = runBlocking {
         // Arrange
         val cart = listOf("001", "002", "003", "004")
         Mockito.`when`(productRepository.getProducts(cart)).thenReturn(getProducts())
 
         // Act
-        val price = CheckoutPriceCalculatorUseCase(cart, productRepository).calculate()
+        val price = checkoutPriceCalculatorUseCase.calculate(cart)
 
         // Assert
-        Assertions.assertThat(price).isEqualTo(260f)
+        Assertions.assertThat(price).isEqualTo(BigDecimal(260))
+
+        Unit
     }
 
     @Test
-    fun calculate_whenDiscountAppliesForOneItemOnly() {
+    fun calculate_whenDiscountAppliesForOneItemOnly(): Unit = runBlocking {
         // Arrange
         val cart = listOf("001", "002", "002", "004", "003")
         Mockito.`when`(productRepository.getProducts(cart)).thenReturn(getProducts())
 
         // Act
-        val price = CheckoutPriceCalculatorUseCase(cart, productRepository).calculate()
+        val price = checkoutPriceCalculatorUseCase.calculate(cart)
 
         // Assert
-        Assertions.assertThat(price).isEqualTo(300f)
+        Assertions.assertThat(price).isEqualTo(BigDecimal(300))
+
+        Unit
     }
 
     @Test
-    fun calculate_whenDiscountAppliesForTwoItems() {
+    fun calculate_whenDiscountAppliesForTwoItems(): Unit = runBlocking {
         // Arrange
         val cart = listOf("001", "001", "001", "002", "002", "004", "003")
         Mockito.`when`(productRepository.getProducts(cart)).thenReturn(getProducts())
 
         // Act
-        val price = CheckoutPriceCalculatorUseCase(cart, productRepository).calculate()
+        val price = checkoutPriceCalculatorUseCase.calculate(cart)
 
         // Assert
-        Assertions.assertThat(price).isEqualTo(400f)
+        Assertions.assertThat(price).isEqualTo(BigDecimal(400))
+
+        Unit
     }
 
     private fun getProducts(): List<Product> {
         return listOf(
-                Product("001", "Rolex", 100f, discountPrice = 200f, discountQty = 3),
-                Product("002", "Michael Kors", 80f, discountPrice = 120f, discountQty = 2),
-                Product("003", "Swatch", 50f),
-                Product("004", "Casio", 30f)
+                Product("001", "Rolex", BigDecimal(100), discountPrice = BigDecimal(200), discountQty = 3),
+                Product("002", "Michael Kors", BigDecimal(80), discountPrice = BigDecimal(120), discountQty = 2),
+                Product("003", "Swatch", BigDecimal(50)),
+                Product("004", "Casio", BigDecimal(30))
         )
     }
 }
