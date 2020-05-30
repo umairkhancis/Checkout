c82b4a5a6128ea9b1224286234189f3ad2e5227e
Umair Khan <umairkhan.cis@gmail.com>
2020-05-31 01:32:40 +0400

    Change data type of price from float to big decimal.

--- src/main/kotlin/com/noorifytech/maha/core/domain/Product.kt
+++ src/main/kotlin/com/noorifytech/maha/core/domain/Product.kt
@@ -1,9 +1,11 @@
 package com.noorifytech.maha.core.domain
 
+import java.math.BigDecimal
+
 data class Product(
         val id: String,
         val name: String,
-        val unitPrice: Float,
-        val discountPrice: Float = unitPrice,
+        val unitPrice: BigDecimal,
+        val discountPrice: BigDecimal = unitPrice,
         val discountQty: Int = 1
 )
--- src/test/kotlin/com/noorifytech/maha/core/domain/ProductTest.kt
+++ src/test/kotlin/com/noorifytech/maha/core/domain/ProductTest.kt
@@ -2,11 +2,12 @@ package com.noorifytech.maha.core.domain
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 
 class ProductTest {
     @Test
-    fun test() {
-        val watch = Product("001", "Rolex", 100f)
+    fun creation() {
+        val watch = Product("001", "Rolex", BigDecimal(100))
         Assertions.assertThat(watch.id).isEqualTo("001")
     }
 }
